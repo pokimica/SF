6a04233d1cc479f8e1b36fcb1a4af191755b8c1e
Saša Pokimica <sasapokimica@yahoo.com>
2018-10-19 13:52:10 +0200

    sas

--- sasapokimica/src/aura/LogoutButton/LogoutButtonController.js
+++ sasapokimica/src/aura/LogoutButton/LogoutButtonController.js
@@ -1,5 +1,5 @@
 ({
-	logout : function(component, event, helper) {
+	Logout : function(component, event, helper) {
 		window.location.replace("https://sasapokimica-developer-edition.eu16.force.com/servlet/networks/switch?startURL=%2Fsecur%2Flogout.jsp")
 	}
 })
