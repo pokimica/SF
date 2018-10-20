4be374fbf16dc9b83dfbe7e7b240528760751902
Saša Pokimica <sasapokimica@yahoo.com>
2018-10-20 16:23:01 +0200

    Changes

--- sasapokimica/src/aura/verticalNav/verticalNavController.js
+++ sasapokimica/src/aura/verticalNav/verticalNavController.js
@@ -1,5 +1,8 @@
 ({
-	myAction : function(component, event, helper) {
-		
+	onClick : function(component, event, helper) {
+		var id = event.target.dataset.menuItemId;
+		if (id) {
+			component.getSuper().navigate(id);
+        }
 	}
 })
